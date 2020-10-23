f6d04268436a43bed34a2c51194df0fac4bbc066
saee <saee_saadat@outlook.com>
2020-10-23 19:26:53 +0330

    node sha256

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const sha256 = require('js-sha256');
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+app.listen(3000, () => console.log('Node backend is Running! Waiting for your command sir!'));
+
+app.post('/sha256', (req, res) => {
+    const data = req.body;
+    const a = data.a;
+    const b = data.b;
+    const c = a + b;
+    res.json({
+        c: sha256(`${c}`)
+    });
+    // res.json({
+    //     c: sha256(c)
+    // });
+})
+
+app.get('/index', (request, response) => {
+    response.json({
+        body: "<h1> salam <h1/>"
+    });
+})
