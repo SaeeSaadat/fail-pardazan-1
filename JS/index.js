8a693d6893ff5e48974724cf874a0461d4d1daa9
saee <saee_saadat@outlook.com>
2020-10-23 20:41:16 +0330

    error handling

--- JS/index.js
+++ JS/index.js
@@ -6,12 +6,18 @@ app.use(express.json());
 
 app.listen(3000, () => console.log('Node backend is Running! Waiting for your command sir!'));
 
-app.post('nodejs/sha256', (req, res) => {
+app.post('/nodejs/sha256', (req, res) => {
+    console.log("POST request for sha256 received");
     const data = req.body;
     const a = data.a;
     const b = data.b;
+    if (!(!isNaN(a) && !isNaN(b))) {
+        res.status(400).send({message: "user inputs must be numbers"});
+        return;
+    }
     const c = a + b;
     res.json({
         c: sha256(`${c}`)
     });
+    res.send();
 })
