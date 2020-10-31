e4d015e4de8bc153b0791fb80c2a7c29058686c6
Saee <saee_saadat@outlook.com>
2020-10-31 18:49:37 +0330

    nginx.conf

--- JS/index.js
+++ JS/index.js
@@ -41,5 +41,6 @@ app.get('/nodejs/write', (req, res) => {
     }).catch((error) => {
         console.error(error);
         return res.status(500).send({message: "couldn't read the line!"});
-    })
+    });
+    res.end();
 })
